0bef4faa858ab46cfe7698cc7cd8d0bb24f9b67f
Константин Бобков <b.konstantv@gmail.com>
2023-07-21 22:52:32 +0300

    After review 3

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
-
-sudo docker compose -f docker-compose.production.yml exec backend python manage.py migrate 
-sudo docker compose -f docker-compose.production.yml exec backend python manage.py collectstatic 
-sudo docker compose -f docker-compose.production.yml exec backend cp -r /app/collected_static/. /backend_static/static/ 
+cd backend
+python manage.py migrate 
+python manage.py collectstatic 
+cp -r /app/collected_static/. /backend_static/static/ 
