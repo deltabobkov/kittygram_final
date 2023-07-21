c9497b34eef8fa85cbae0d728451b07a9e14842d
Константин Бобков <b.konstantv@gmail.com>
2023-07-21 19:41:09 +0300

    After review

---
+++ backend/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+
+sudo docker compose -f docker-compose.production.yml exec backend python manage.py migrate 
+sudo docker compose -f docker-compose.production.yml exec backend python manage.py collectstatic 
+sudo docker compose -f docker-compose.production.yml exec backend cp -r /app/collected_static/. /backend_static/static/ 
--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,13 +3,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = (
-    "django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$"
-)
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["ppcaskitty.ddns.net"]
+ALLOWED_HOSTS = os.getenv("HOSTS").split(',')
 
 INSTALLED_APPS = [
     "django.contrib.admin",
