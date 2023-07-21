fa546e50e8279984a5af71fc964ac97fae2eab4c
Константин Бобков <b.konstantv@gmail.com>
2023-07-21 19:56:02 +0300

    After review 2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = os.getenv("HOSTS").split(',')
+HOSTS : str = os.getenv("HOSTS", 'localhost,127.0.0.1') 
+ALLOWED_HOSTS = HOSTS.split(',')
 
 INSTALLED_APPS = [
     "django.contrib.admin",
