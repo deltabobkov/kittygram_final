af3c2aca994454cd962dd70247277b68326a2a11
Константин Бобков <b.konstantv@gmail.com>
2023-07-22 22:50:10 +0300

    After second review 2

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "key")
 
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG") == "True"
 
 HOSTS: str = os.getenv("HOSTS", "127.0.0.1, localhost")
 ALLOWED_HOSTS = HOSTS.split(',')
