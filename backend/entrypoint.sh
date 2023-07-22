dc5e7b14ed0bc4bf60eb155bd67c56b9d5d9ce92
Константин Бобков <b.konstantv@gmail.com>
2023-07-22 23:09:25 +0300

    After second review 2

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,3 +3,4 @@ cd backend
 python manage.py migrate --noinput
 python manage.py collectstatic 
 cp -rf /app/collected_static/. /backend_static/static/ 
+gunicorn --bind 0.0.0.0:9000 kittygram_backend.wsgi
